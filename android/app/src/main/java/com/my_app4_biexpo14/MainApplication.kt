136c8ed6e00d7c5106aa63e36acba1a432b2335c
Reza <82436046+Reza-kavian@users.noreply.github.com>
2025-11-06 13:07:36 +0330

    fix: expo-application Gradle patch + pluginManagement setup

--- android/app/src/main/java/com/my_app4_biexpo14/MainApplication.kt
+++ android/app/src/main/java/com/my_app4_biexpo14/MainApplication.kt
@@ -6,9 +6,9 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.swmansion.reanimated.ReanimatedJSIModulePackage
 
 class MainApplication : Application(), ReactApplication {
-
   override val reactHost: ReactHost by lazy {
     getDefaultReactHost(
       context = applicationContext,
@@ -16,10 +16,10 @@ class MainApplication : Application(), ReactApplication {
         PackageList(this).packages.apply {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // add(MyReactNativePackage())
+          add(ReanimatedJSIModulePackage())
         },
     )
   }
-
   override fun onCreate() {
     super.onCreate()
     loadReactNative(this)
